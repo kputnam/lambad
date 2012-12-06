a085d2d075c65c5f89b6e77693aec56a858372a3
kputnam <235798+kputnam@users.noreply.github.com>
2012-12-05 21:04:36 -0600

    Utilities to make REPL (GHCi) easier

---
+++ src/Language/Lambad/Misc.hs
@@ -0,0 +1,13 @@
+module Language.Lambad.Misc
+  ( lmap
+  , swap
+  ) where
+
+import Control.Monad.Instances
+import Data.Either
+
+swap :: Either a b -> Either b a
+swap = either Right Left
+
+lmap :: (a -> b) -> Either a c -> Either b c
+lmap f = swap . fmap f . swap
