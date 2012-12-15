d2d2e89ede825e08ae18e095299fd42c54267690
kputnam <235798+kputnam@users.noreply.github.com>
2012-12-15 03:52:50 -0600

    Supress redundant parentheses in pretty-printed output

--- src/Language/Pure/Pretty.hs
+++ src/Language/Pure/Pretty.hs
@@ -25,11 +25,27 @@ instance Pretty Declaration where
     = parens $ text "define" <+> text (unpack x) <+> pretty e
 
 instance Pretty Expression where
+  -- Render (λx.x) (λx.x) instead of λx.x λx.x,
+  pretty (Application f@(Abstraction _ _) x@(Abstraction _ _))
+    = parens (pretty f) <+> parens (pretty x)
+
+  -- Render (λx.x) e instead of λx. x e
+  pretty (Application f@(Abstraction _ _) x)
+    = parens (pretty f) <+> pretty x
+
+  -- Render f (λx.x) instead of f λ.x x
+  pretty (Application f x@(Abstraction _ _))
+    = pretty f <+> parens (pretty x)
+
+  -- Render f (g x) instead of f g x
+  pretty (Application f x@(Application _ _))
+    = pretty f <+> parens (pretty x)
+
   pretty (Application f x)
-    = parens $ pretty f <+> pretty x
+    = pretty f <+> pretty x
 
   pretty (Abstraction x e)
-    = parens $ text "λ" <> text (unpack x) <> text "." <+> pretty e
+    = text "λ" <> text (unpack x) <> text "." <+> pretty e
 
   pretty (Variable x)
     = text (unpack x)
