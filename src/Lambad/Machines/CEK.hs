57e2b4ec1cd8f1e857151b68b02cdc3e1af3bc48
kputnam <235798+kputnam@users.noreply.github.com>
2013-01-02 16:39:24 -0600

    Implement CEK machine

---
+++ src/Lambad/Machines/CEK.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE UnicodeSyntax #-}
+
+module Lambad.Machines.CEK
+  ( eval
+  ) where
+
+import qualified Data.Map as M
+import Data.Text (Text)
+import Data.Maybe
+
+import Lambad.Pure.Syntax
+
+type Id
+  = Text
+
+type Program
+  = Expression
+
+-- C: Control
+data Value
+  = Closure Id Program Environment
+  | Unbound Id
+  deriving (Show)
+
+-- E: Environment
+type Environment
+  = M.Map Id Value
+
+-- K: Continuation
+data Frame
+  = Stop
+  | Operand Value Frame                 -- need an operand
+  | Operator Program Environment Frame  -- need an operator
+  deriving (Show)
+
+type State
+  = (Program, Environment, Frame)
+
+eval ∷ Program → Value
+eval
+  = uncurry lift . two . fold . inject
+  where
+    final (Abstraction _ _, _, Stop) = True
+    final _                          = False
+
+    fold s | final s   = s
+           | otherwise = fold (step s)
+
+    two (a, b, c) = (a, b)
+
+inject ∷ Program → State
+inject e
+  = (e, M.empty, Stop)
+
+lift ∷ Program → Environment → Value
+lift (Abstraction x b)
+  = Closure x b
+
+step ∷ State → State
+
+-- Lookup references in the environment ρ
+step e@(Variable x, ρ, κ)
+  = maybe e unlift $ M.lookup x ρ
+  where
+    unlift (Closure x b ρ) = (Abstraction x b, ρ, κ)
+
+-- For application, first evaluate the operator
+step (Application f a, ρ, κ)
+  = (f, ρ, Operator a ρ κ)
+
+-- We have an operator, next evaluate the operand
+step (Abstraction x b, ρ, Operator a ρ' κ)
+  = (a, ρ', Operand (Closure x b ρ) κ)
+
+-- We have an operand, next evaluate operand body
+step (e, ρ, Operand (Closure x' b' ρ') κ)
+  = (b', M.insert x' (lift e ρ) ρ', κ)
